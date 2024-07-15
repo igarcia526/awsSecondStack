8d01b70b4ed2fb76b06077219566f7401eaf6761
Isaac <garciai0526@hotmail.com>
2024-07-15 18:07:50 -0400

    test axios

--- lib/aws_second_stack-stack.ts
+++ lib/aws_second_stack-stack.ts
@@ -1,5 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsSecondStackStack extends cdk.Stack {
@@ -12,5 +17,13 @@ export class AwsSecondStackStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'AwsSecondStackQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const myLambda = new  lambda.Function(this, 'izzys-second-lambda', {
+      functionName: 'myLambda',
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lib/service/runtime'),
+    });
+
   }
 }
